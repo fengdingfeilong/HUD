8be2ed4deb67dfa9507697f4d00fdfc4cade99d1
fengdingfeilong <1374084580@qq.com>
2016-07-13 19:03:54 +0800

    Add files via upload

--- HUD/HUDControl.xaml.cs
+++ HUD/HUDControl.xaml.cs
@@ -102,6 +102,7 @@ namespace HUD
                 left_tl.Stroke = Brushes.White;
                 left_tl.StrokeThickness = 1;
                 yawstaffCanvas.Children.Add(left_tl);
+                if ((isrightcanvas && d % 2 == 1) || (!isrightcanvas && d % 2 == 0)) continue;//间隔显示
                 var ticktext = new BorderTextLabel();
                 ticktext.FontWeight = FontWeights.ExtraBold;
                 ticktext.Stroke = Brushes.Gray;
@@ -202,7 +203,7 @@ namespace HUD
             textblock.Foreground = Brushes.White;
             textblock.FontSize = 16;
             textblock.FontWeight = FontWeights.Bold;
-            Canvas.SetTop(textblock, offset - 11);
+            Canvas.SetTop(textblock, offset - 8);
             Canvas.SetLeft(textblock, -textblock.Width - 26);
             Canvas_ViewPortMiddle.Children.Add(textblock);
         }
@@ -246,7 +247,8 @@ namespace HUD
         {
             Canvas_ViewPortMiddle.Children.Clear();
             bool isLargeArc = MaxRollAngle > 90;
-            double cycleR = Grid_Virwport.ActualWidth / 4;
+            //double cycleR = Grid_Virwport.ActualWidth / 4;
+            double cycleR = 100;
             Point startPoint = new Point(-cycleR * Math.Sin(MaxRollAngle * Math.PI / 180), -cycleR * Math.Cos(MaxRollAngle * Math.PI / 180));
             Point endPoint = new Point(cycleR * Math.Sin(MaxRollAngle * Math.PI / 180), -cycleR * Math.Cos(MaxRollAngle * Math.PI / 180));
             if (MaxRollAngle == 180)
@@ -348,12 +350,350 @@ namespace HUD
             DrawRollPitchCycle();
         }
 
+        #region 其他杂项
+        /// <summary>
+        /// 海拔（高度）
+        /// </summary>
+        public double FlyHeight
+        {
+            get { return (double)GetValue(FlyHeightProperty); }
+            set { SetValue(FlyHeightProperty, value); }
+        }
+        public static readonly DependencyProperty FlyHeightProperty =
+            DependencyProperty.Register("FlyHeight", typeof(double), typeof(HUDControl), new FrameworkPropertyMetadata((double)0, GestureChangedCallback));
+        /// <summary>
+        /// 空速
+        /// </summary>
+        public double AirSpeed
+        {
+            get { return (double)GetValue(AirSpeedProperty); }
+            set { SetValue(AirSpeedProperty, value); }
+        }
+        public static readonly DependencyProperty AirSpeedProperty =
+            DependencyProperty.Register("AirSpeed", typeof(double), typeof(HUDControl), new FrameworkPropertyMetadata((double)0, GestureChangedCallback));
+        /// <summary>
+        /// 地速
+        /// </summary>
+        public double GroundSpeed
+        {
+            get { return (double)GetValue(GroundSpeedProperty); }
+            set { SetValue(GroundSpeedProperty, value); }
+        }
+        public static readonly DependencyProperty GroundSpeedProperty =
+            DependencyProperty.Register("GroundSpeed", typeof(double), typeof(HUDControl), new FrameworkPropertyMetadata((double)0, GestureChangedCallback));
+        /// <summary>
+        /// 电压
+        /// </summary>
+        public double Voltage
+        {
+            get { return (double)GetValue(VoltageProperty); }
+            set { SetValue(VoltageProperty, value); }
+        }
+        public static readonly DependencyProperty VoltageProperty =
+            DependencyProperty.Register("Voltage", typeof(double), typeof(HUDControl), new FrameworkPropertyMetadata((double)0, GestureChangedCallback));
+        /// <summary>
+        /// 电流
+        /// </summary>
+        public double Galvanic
+        {
+            get { return (double)GetValue(GalvanicProperty); }
+            set { SetValue(GalvanicProperty, value); }
+        }
+        public static readonly DependencyProperty GalvanicProperty =
+            DependencyProperty.Register("Galvanic", typeof(double), typeof(HUDControl), new FrameworkPropertyMetadata((double)0, GestureChangedCallback));
+        /// <summary>
+        /// 剩余电量百分比
+        /// </summary>
+        public double BatteryPercent
+        {
+            get { return (double)GetValue(BatteryPercentProperty); }
+            set { SetValue(BatteryPercentProperty, value); }
+        }
+        public static readonly DependencyProperty BatteryPercentProperty =
+            DependencyProperty.Register("BatteryPercent", typeof(double), typeof(HUDControl), new FrameworkPropertyMetadata((double)1, GestureChangedCallback));
+        /// <summary>
+        /// 飞行时间
+        /// </summary>
+        public int FlyTime
+        {
+            get { return (int)GetValue(FlyTimeProperty); }
+            set { SetValue(FlyTimeProperty, value); }
+        }
+        public static readonly DependencyProperty FlyTimeProperty =
+            DependencyProperty.Register("FlyTime", typeof(int), typeof(HUDControl), new FrameworkPropertyMetadata((int)0, GestureChangedCallback));
+        /// <summary>
+        /// 是否有EKF
+        /// </summary>
+        public bool HasEKF
+        {
+            get { return (bool)GetValue(HasEKFProperty); }
+            set { SetValue(HasEKFProperty, value); }
+        }
+        public static readonly DependencyProperty HasEKFProperty =
+            DependencyProperty.Register("HasEKF", typeof(bool), typeof(HUDControl), new FrameworkPropertyMetadata((bool)true, GestureChangedCallback));
+        /// <summary>
+        /// 是否有Vibe
+        /// </summary>
+        public bool HasVibe
+        {
+            get { return (bool)GetValue(HasVibeProperty); }
+            set { SetValue(HasVibeProperty, value); }
+        }
+        public static readonly DependencyProperty HasVibeProperty =
+            DependencyProperty.Register("HasVibe", typeof(bool), typeof(HUDControl), new FrameworkPropertyMetadata((bool)true, GestureChangedCallback));
+        /// <summary>
+        /// 是否有GPS
+        /// </summary>
+        public bool HasGPS
+        {
+            get { return (bool)GetValue(HasGPSProperty); }
+            set { SetValue(HasGPSProperty, value); }
+        }
+        public static readonly DependencyProperty HasGPSProperty =
+            DependencyProperty.Register("HasGPS", typeof(bool), typeof(HUDControl), new FrameworkPropertyMetadata((bool)false, GestureChangedCallback));
+
+
+        //绘制右边海拔（高度）矩形
+        private void RedrawHeight()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Width = 50;
+            rectangle.Height = 150;
+            rectangle.StrokeThickness = 2;
+            rectangle.Stroke = Brushes.White;
+            Canvas.SetTop(rectangle, -rectangle.Height / 2);
+            Canvas.SetRight(rectangle, 0);
+            Canvas_ViewPortRight.Children.Add(rectangle);
+            double from = FlyHeight - FlyHeight % 5 + 15;
+            double space = (rectangle.Height - 20) / 5;
+            for (int i = 0; i < 6; i++)
+            {
+                Line li = new Line();
+                li.X1 = -rectangle.Width;
+                li.Y1 = -rectangle.Height / 2 + 10 + space * i;
+                li.X2 = li.X1 + 10;
+                li.Y2 = li.Y1;
+                li.StrokeThickness = 2;
+                li.Stroke = Brushes.White;
+                Canvas_ViewPortRight.Children.Add(li);
+                BorderTextLabel texti = new BorderTextLabel();
+                texti.Width = 22;
+                texti.Stroke = Brushes.DimGray;
+                texti.HorizontalContentAlignment = HorizontalAlignment.Left;
+                texti.Text = (from - i * 5).ToString("##0");
+                texti.Foreground = Brushes.White;
+                texti.FontSize = 16;
+                texti.FontWeight = FontWeights.Bold;
+                Canvas.SetLeft(texti, li.X2 + 4);
+                Canvas.SetTop(texti, li.Y1 - 10);
+                Canvas_ViewPortRight.Children.Add(texti);
+            }
+
+            Line l1 = new Line();
+            l1.X1 = -rectangle.Width + 1;
+            l1.Y1 = -rectangle.Height / 2 + 1;
+            l1.X2 = l1.X1 - 16;
+            l1.Y2 = l1.Y1 + 16;
+            l1.StrokeThickness = 2;
+            l1.Stroke = Brushes.White;
+            Canvas_ViewPortRight.Children.Add(l1);
+            Line l2 = new Line();
+            l2.X1 = l1.X2;
+            l2.Y1 = l1.Y2 - 1;
+            l2.X2 = l2.X1;
+            l2.Y2 = l2.Y1 + rectangle.Height - 32;
+            l2.StrokeThickness = 2;
+            l2.Stroke = Brushes.White;
+            Canvas_ViewPortRight.Children.Add(l2);
+            Line l3 = new Line();
+            l3.X1 = l2.X2;
+            l3.Y1 = l2.Y2 - 1;
+            l3.X2 = l1.X1;
+            l3.Y2 = l1.Y1 + rectangle.Height - 2;
+            l3.StrokeThickness = 2;
+            l3.Stroke = Brushes.White;
+            Canvas_ViewPortRight.Children.Add(l3);
+            space = (rectangle.Height - 36) / 9;
+            for (int i = 0; i < 10; i++)
+            {
+                Line li = new Line();
+                li.X1 = l1.X2;
+                li.X2 = li.X1 + 8;
+                li.Y1 = l1.Y2 + 2 + space * i;
+                li.Y2 = li.Y1;
+                li.StrokeThickness = 2;
+                li.Stroke = Brushes.White;
+                Canvas_ViewPortRight.Children.Add(li);
+            }
+
+            TextBlock textblock = new TextBlock();
+            textblock.Width = 30;
+            textblock.Padding = new Thickness(0, 0, 2, 0);
+            textblock.TextAlignment = TextAlignment.Right;
+            textblock.Text = FlyHeight.ToString("##0.#");
+            textblock.Foreground = Brushes.White;
+            textblock.Background = Brushes.Red;
+            textblock.FontSize = 12;
+            textblock.FontWeight = FontWeights.Bold;
+            double offset = (from - FlyHeight) / 25 * (rectangle.Height - 20) - rectangle.Height / 2 + 10;
+            Canvas.SetTop(textblock, offset - 8);
+            Canvas.SetRight(textblock, 0);
+            Canvas_ViewPortRight.Children.Add(textblock);
+        }
+
+        //绘制左边速度（空速 ）矩形
+        private void RedrawSpeed()
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.Width = 50;
+            rectangle.Height = 150;
+            rectangle.StrokeThickness = 2;
+            rectangle.Stroke = Brushes.White;
+            Canvas.SetTop(rectangle, -rectangle.Height / 2);
+            Canvas.SetLeft(rectangle, 0);
+            Canvas_ViewPortLeft.Children.Add(rectangle);
+            double from = AirSpeed - AirSpeed % 5 + 15;
+            if (from < 25) from = 25;
+            double space = (rectangle.Height - 20) / 5;
+            for (int i = 0; i < 6; i++)
+            {
+                Line li = new Line();
+                li.X1 = rectangle.Width;
+                li.Y1 = -rectangle.Height / 2 + 10 + space * i;
+                li.X2 = li.X1 - 10;
+                li.Y2 = li.Y1;
+                li.StrokeThickness = 2;
+                li.Stroke = Brushes.White;
+                Canvas_ViewPortLeft.Children.Add(li);
+                BorderTextLabel texti = new BorderTextLabel();
+                texti.Width = 22;
+                texti.Stroke = Brushes.DimGray;
+                texti.HorizontalContentAlignment = HorizontalAlignment.Right;
+                texti.Text = (from - i * 5).ToString("##0");
+                texti.Foreground = Brushes.White;
+                texti.FontSize = 16;
+                texti.FontWeight = FontWeights.Bold;
+                Canvas.SetLeft(texti, li.X2 - texti.Width - 4);
+                Canvas.SetTop(texti, li.Y1 - 10);
+                Canvas_ViewPortLeft.Children.Add(texti);
+            }
+
+            TextBlock textblock = new TextBlock();
+            textblock.Width = 30;
+            textblock.Padding = new Thickness(2, 0, 0, 0);
+            textblock.TextAlignment = TextAlignment.Left;
+            textblock.Text = AirSpeed.ToString("##0.#");
+            textblock.Foreground = Brushes.White;
+            textblock.Background = Brushes.Red;
+            textblock.FontSize = 12;
+            textblock.FontWeight = FontWeights.Bold;
+            double offset = (from - AirSpeed) / 25 * (rectangle.Height - 20) - rectangle.Height / 2 + 10;
+            Canvas.SetTop(textblock, offset - 8);
+            Canvas.SetLeft(textblock, 0);
+            Canvas_ViewPortLeft.Children.Add(textblock);
+
+        }
+        private void RedrawOthersInfoText()
+        {
+            BorderTextLabel airspeedlabel = new BorderTextLabel();
+            airspeedlabel.Stroke = Brushes.DimGray;
+            airspeedlabel.Foreground = Brushes.White;
+            airspeedlabel.FontSize = 14;
+            airspeedlabel.FontWeight = FontWeights.Bold;
+            airspeedlabel.Text = "空速  " + AirSpeed.ToString("0.0");
+            Canvas.SetLeft(airspeedlabel, 4);
+            Canvas.SetTop(airspeedlabel, 80);
+            Canvas_ViewPortLeft.Children.Add(airspeedlabel);
+
+            BorderTextLabel groundspeedlabel = new BorderTextLabel();
+            groundspeedlabel.Stroke = Brushes.DimGray;
+            groundspeedlabel.Foreground = Brushes.White;
+            groundspeedlabel.FontSize = 14;
+            groundspeedlabel.FontWeight = FontWeights.Bold;
+            groundspeedlabel.Text = "地速  " + GroundSpeed.ToString("0.0");
+            Canvas.SetLeft(groundspeedlabel, 4);
+            Canvas.SetTop(groundspeedlabel, 100);
+            Canvas_ViewPortLeft.Children.Add(groundspeedlabel);
+
+            BorderTextLabel batteryabel = new BorderTextLabel();
+            batteryabel.Stroke = Brushes.DimGray;
+            batteryabel.Foreground = Brushes.White;
+            batteryabel.FontSize = 14;
+            batteryabel.FontWeight = FontWeights.Bold;
+            batteryabel.Text = string.Format("电池  {0} v  {1} A  {2}%", Voltage.ToString("0.00"), Galvanic.ToString("0.0"), BatteryPercent * 100);
+            Canvas.SetLeft(batteryabel, 10);
+            Canvas.SetTop(batteryabel, 130);
+            Canvas_ViewPortLeft.Children.Add(batteryabel);
+
+            if (HasEKF)
+            {
+                BorderTextLabel ekflabel = new BorderTextLabel();
+                ekflabel.Stroke = Brushes.DimGray;
+                ekflabel.Foreground = Brushes.White;
+                ekflabel.FontSize = 14;
+                ekflabel.FontWeight = FontWeights.Bold;
+                ekflabel.Text = "EKF";
+                Canvas.SetLeft(ekflabel, 200);
+                Canvas.SetTop(ekflabel, 130);
+                Canvas_ViewPortLeft.Children.Add(ekflabel);
+            }
+            if (HasVibe)
+            {
+                BorderTextLabel vibelabel = new BorderTextLabel();
+                vibelabel.Stroke = Brushes.DimGray;
+                vibelabel.Foreground = Brushes.Red;
+                vibelabel.FontSize = 14;
+                vibelabel.FontWeight = FontWeights.Bold;
+                vibelabel.Text = "Vibe";
+                Canvas.SetLeft(vibelabel, 250);
+                Canvas.SetTop(vibelabel, 130);
+                Canvas_ViewPortLeft.Children.Add(vibelabel);
+            }
+
+            BorderTextLabel gpslabel = new BorderTextLabel();
+            gpslabel.Stroke = Brushes.DimGray;
+            gpslabel.Foreground = Brushes.Red;
+            gpslabel.FontSize = 14;
+            gpslabel.FontWeight = FontWeights.Bold;
+            string s = HasGPS ? "有GPS" : "无GPS";
+            gpslabel.Text = "GPS: " + s;
+            Canvas.SetLeft(gpslabel, 300);
+            Canvas.SetTop(gpslabel, 130);
+            Canvas_ViewPortLeft.Children.Add(gpslabel);
+
+            BorderTextLabel flytimelabel = new BorderTextLabel();
+            flytimelabel.Stroke = Brushes.DimGray;
+            flytimelabel.Foreground = Brushes.White;
+            flytimelabel.FontSize = 14;
+            flytimelabel.FontWeight = FontWeights.Bold;
+            flytimelabel.Text = new TimeSpan(FlyTime).ToString(@"hh\:mm\:ss");
+            Canvas.SetLeft(flytimelabel, -80);
+            Canvas.SetTop(flytimelabel, -110);
+            Canvas_ViewPortRight.Children.Add(flytimelabel);
+
+
+        }
+        private void RedrawOthers()
+        {
+            Canvas_ViewPortLeft.Children.Clear();
+            Canvas_ViewPortRight.Children.Clear();
+
+            RedrawHeight();
+            RedrawSpeed();
+            RedrawOthersInfoText();
+        }
+
+        #endregion
+
+
         protected override void OnRender(DrawingContext drawingContext)
         {
             base.OnRender(drawingContext);
             RedrawYaw();
             RedrawRoll();
             RedrawPitch();
+
+            RedrawOthers();//绘制高度、速度等
         }
 
     }
